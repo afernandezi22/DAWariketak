3ef07e9a83a46934051d03976e89b92fa53ff179
asierfernandez <afernandezi22@fpsanjorge.com>
2024-01-25 23:18:03 +0100

    Ariketa 3 proiektu

---
+++ Bezero/Ariketa3-ErabiltzaileenTaula/vue-project/src/components/CreadorTaula.vue
@@ -0,0 +1,14 @@
+<script setup>
+    defineProps(["data"]);
+</script>
+
+<template>
+    <tr v-for="pertsona in data">
+        <td>{{ pertsona.ID }}</td>
+        <td>{{ pertsona.Izena }}</td>
+        <td>{{ pertsona.Abizena }}</td>
+        <td>{{ pertsona.NAN }}</td>
+        <td>{{ pertsona.JaiotzeData }}</td>
+        <td>{{ pertsona.Adina }}</td>
+    </tr>
+</template>
